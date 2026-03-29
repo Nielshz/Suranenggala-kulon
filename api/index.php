b7a19468c624133d1620ba2a42165f49e55e5299
Nielshz <Nielshz@users.noreply.github.com>
2026-03-29 17:30:05 +0700

    Force APP_DEBUG to trace 500 error

--- api/index.php
+++ api/index.php
@@ -9,6 +9,7 @@ $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 $_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['APP_STORAGE'] = '/tmp';
+$_ENV['APP_DEBUG'] = 'true'; // FORCE DEBUG MODE
 
 // Memaksa cache framework ke /tmp
 putenv('VIEW_COMPILED_PATH=/tmp');
